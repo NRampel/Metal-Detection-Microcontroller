db96086f9e5dd7fd9156348b7c39266e9978a1d2
Nicolas <nrampelotto414@gmail.com>
2026-04-30 10:19:41 -0400

    Fixed merge conflict

--- WP/_ide/workspace_journal.py
+++ WP/_ide/workspace_journal.py
@@ -1,8 +1,14 @@
-# 2026-04-30T09:56:50.489364600
+# 2026-04-30T10:10:54.062156800
 import vitis
 
 client = vitis.create_client()
 client.set_workspace(path="WP")
 
-vitis.dispose()
+comp = client.create_app_component(name="MD_app",platform = "$COMPONENT_LOCATION/../MD_plat/export/MD_plat/MD_plat.xpfm",domain = "standalone_microblaze_0")
+
+client.delete_component(name="MD_app")
+
+client.delete_component(name="componentName")
+
+comp = client.create_app_component(name="MD_app",platform = "$COMPONENT_LOCATION/../MD_plat/export/MD_plat/MD_plat.xpfm",domain = "standalone_microblaze_0",template = "hello_world")
 
---
+++ WP/_ide/workspace_journal_33324.py
@@ -0,0 +1,8 @@
+# 2026-04-30T09:56:50.489364600
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="WP")
+
+vitis.dispose()
+
